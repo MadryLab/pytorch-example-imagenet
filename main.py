a61c602fb43e7a4a4a70dd907033fe07f09038ab
Logan Engstrom <engstrom@csail.mit.edu>
2021-10-28 23:08:58 -0400

    added

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ model_names = sorted(name for name in models.__dict__
     and callable(models.__dict__[name]))
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
-parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
+parser.add_argument('-a', '--arch', metavar='ARCH', default=os.environ['ARCH'],
                     choices=model_names,
                     help='model architecture: ' +
                         ' | '.join(model_names) +
